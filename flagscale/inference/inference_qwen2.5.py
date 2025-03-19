888e197b4ad8415cf69972f130f00136ef1d466d
chenzhuo <chenzhuo@tju.edu.cn>
2025-03-19 19:16:55 +0800

    infer qwen2.5

---
+++ flagscale/inference/inference_qwen2.5.py
@@ -0,0 +1,25 @@
+import vllm
+from vllm import LLM
+from vllm.sampling_params import SamplingParams
+
+from flagscale.inference.arguments import parse_config
+
+
+def inference_qwen(config):
+
+    prompts = config.generate.get("prompts", "")
+
+    llm_cfg = config.get("llm", {})
+    llm = LLM(**llm_cfg)
+
+    sampling_cfg = config.generate.get("sampling", {})
+    sampling_params = SamplingParams(**sampling_cfg)
+
+    outputs = llm.generate(prompts, sampling_params)
+    print("generate result ", outputs, flush=True)
+
+
+if __name__ == "__main__":
+    config = parse_config()
+    print(f"[vllm.__file__] {vllm.__file__}")
+    inference_qwen(config)
